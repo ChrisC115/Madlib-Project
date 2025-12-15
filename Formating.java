577b5d92fa71e6a9fc57b46d61f5461d45581d48
BernardLoquias <bernardclaudiloquias@icloud.com>
2025-12-15 13:27:09 +0000

    commit

---
+++ Formating.java
@@ -0,0 +1,5 @@
+System.out.println("There was a " + adj + noun + "who " + verb + "."/n
+                    "It's name was " + name1 + "and they really " + adj + verb + "every day."/n
+                    "One day their friend " + name2 + verb + "and started crying intensely."/n
+                    "To comfort " + name2 + "they " + verb + "together until they were done."/n
+                    "After a while " + name1 + "and " + name2 + "went to the " + noun + ", and never came back.")

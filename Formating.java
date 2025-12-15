d35a8f3fbbe18404ffb633084731d3297c3f9e23
ChrisC115 <229322366+ChrisC115@users.noreply.github.com>
2025-12-15 13:36:22 +0000

    2

---
+++ Formating.java
@@ -0,0 +1,5 @@
+System.out.println("There was a " + adj1 + noun1 + "who " + verb1 + "."/n
+                    "Their name was " + name1 + "and they really " + adj2 + verb2 + "every day."/n
+                    "One day their friend " + name2 + verb3 + "and then started crying intensely."/n
+                    "To comfort " + name2 + "they " + verb4 + "together until they were done."/n
+                    "After a while " + name1 + "and " + name2 + "went to the " + noun3 + ", and never came back.")
--- MadLib.java
+++ MadLib.java
@@ -1,6 +1,19 @@
 public class MadLib
     public static void main(String[] args) {
 
-        str 
+            System.out.println("Enter an emotion");
+            String adj1 = input.nextLine();
+
+            System.out.println("Enter a name ");
+            String name1 = input.nextLine();
+
+            System.out.println("Enter a verb");
+            String verb1 = input.nextLine();
+
+            System.out.println("Enter a feeling for an action for example enjoyed");
+            String emo2 = input.nextLine();
+
+            System.out.println("Enter an emotion");
+            String emo3 = input.nextLine();
 
     }
